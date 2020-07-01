67c74aecf8587aae1a96c54af79f8bfed8b9ae40
Paul Biberstein <biberstein.paul@gmail.com>
2020-07-01 17:25:26 -0400

    Move projects to front page

--- website2.0.prototype/projects/projects.js
+++ website2.0.prototype/projects/projects.js
@@ -1,3 +1,14 @@
-function main() {}
+function main() {
+  const location = window.location.hash.substr(1);
+
+  const anchors = ["web", "hardware", "acoustic"]
+
+  if (anchors.includes(location)) {
+    window.scrollTo({
+      top: document.getElementById(`${location}-projects`).getBoundingClientRect().top,
+      behavior: "smooth"
+    })
+  }
+}
 
 window.main = main
