5ab582cfc40befb6f2cbc1d3702dfd3cce2da5b0
Paul Biberstein <biberstein.paul@gmail.com>
2020-09-01 22:42:03 -0400

    Add initial blog implementation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,50 @@
 module.exports = {
-    env: {
-      browser: true,
-      es6: true,
-      jest: true
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:react/recommended",
+    "standard",
+    "plugin:prettier/recommended",
+    "prettier/react",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-      "plugin:react/recommended",
-      "plugin:import/errors",
-      "plugin:import/warnings",
-      "plugin:import/typescript",
-      "plugin:react/recommended",
-      "standard",
-      "plugin:prettier/recommended",
-      "prettier/react",
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks"],
+  rules: {
+    "prettier/prettier": [
+      2,
+      {
+        semi: true,
+        singleQuote: false,
+      },
     ],
-    globals: {
-      Atomics: "readonly",
-      SharedArrayBuffer: "readonly",
+    "no-unused-vars": "warn",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+  },
+  settings: {
+    "import/resolver": "webpack",
+    react: {
+      version: "detect",
     },
-    parser: "babel-eslint",
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 11,
-      sourceType: "module",
-    },
-    plugins: ["react", "react-hooks"],
-    rules: {
-      "prettier/prettier": [
-        2,
-        {
-          semi: true,
-          singleQuote: false,
-        },
-      ],
-      "no-unused-vars": "warn",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-    },
-    settings: {
-      "import/resolver": "webpack",
-      react: {
-        version: "detect",
-      },
-    },
-  };
-  
+  },
+};
