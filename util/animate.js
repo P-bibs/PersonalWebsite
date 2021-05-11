300e3793d493c09640ea1ede69041186b7cc3d46
Paul Biberstein <biberstein.paul@gmail.com>
2021-05-11 15:26:14 -0400

    Add delay to animation

--- util/animate.js
+++ util/animate.js
@@ -1,6 +1,8 @@
-const duration = 1000;
+const SINGLE_DURATION = 1000;
+const INITIAL_DELAY = 300;
+const SUBSEQUENT_DELAY = 1000;
 
-export const SCALE = 4;
+export const SCALE = 8;
 
 const easeOutQuint = function (t, b, c, d) {
   t /= d;
@@ -9,15 +11,6 @@ const easeOutQuint = function (t, b, c, d) {
 };
 
 function draw(data, start) {
-  const linearProgress = (new Date().getTime() - start) / duration;
-  if (Math.random() > 0.9) {
-    console.log(`Animation progress: ${linearProgress}`);
-  }
-
-  if (linearProgress > 1) {
-    console.log("Animation finished");
-    return;
-  }
   // initialize canvas
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
@@ -27,36 +20,65 @@ function draw(data, start) {
   ctx.strokeStyle = "rgba(0, 153, 255, 0.4)";
   ctx.save();
 
-  // draw dots
-  Object.keys(data).forEach((iconName, i) =>
-    data[iconName].forEach((props, j) => {
-      const calculatedX = easeOutQuint(
-        linearProgress,
-        props.translateX[0],
-        props.translateX[1] - props.translateX[0],
-        1
-      );
-      const calculatedY = easeOutQuint(
-        linearProgress,
-        props.translateY[0],
-        props.translateY[1] - props.translateY[0],
-        1
-      );
-      const circle = new Path2D();
-
-      circle.arc(
-        calculatedX * SCALE,
-        calculatedY * SCALE,
-        1 * SCALE,
-        0,
-        Math.PI * 2
-      );
-      ctx.fill(circle);
-    })
-  );
+  const elapsedTime = new Date().getTime() - start;
+  if (elapsedTime < INITIAL_DELAY) {
+  } else if (elapsedTime < INITIAL_DELAY + SUBSEQUENT_DELAY) {
+    const linearProgress = (elapsedTime - INITIAL_DELAY) / SINGLE_DURATION;
+    drawCircles(data, "web", linearProgress);
+    drawCircles(data, "hardware", 0);
+    drawCircles(data, "music", 0);
+  } else if (elapsedTime < INITIAL_DELAY + SUBSEQUENT_DELAY * 2) {
+    const linearProgress =
+      (elapsedTime - INITIAL_DELAY - SUBSEQUENT_DELAY) / SINGLE_DURATION;
+    drawCircles(data, "web", 1);
+    drawCircles(data, "hardware", linearProgress);
+    drawCircles(data, "music", 0);
+  } else if (elapsedTime < INITIAL_DELAY + SUBSEQUENT_DELAY * 3) {
+    const linearProgress =
+      (elapsedTime - INITIAL_DELAY - SUBSEQUENT_DELAY * 2) / SINGLE_DURATION;
+    drawCircles(data, "web", 1);
+    drawCircles(data, "hardware", 1);
+    drawCircles(data, "music", linearProgress);
+  } else {
+    drawCircles(data, "web", 1);
+    drawCircles(data, "hardware", 1);
+    drawCircles(data, "music", 1);
+    console.log("Animation finished");
+    return;
+  }
 
   // set up call to next frame
   window.requestAnimationFrame(() => draw(data, start));
+}
+
+function drawCircles(data, iconName, linearProgress) {
+  const ctx = document.getElementById("canvas").getContext("2d");
+
+  // draw dots
+  data[iconName].forEach((props, j) => {
+    const calculatedX = easeOutQuint(
+      linearProgress,
+      props.translateX[0],
+      props.translateX[1] - props.translateX[0],
+      1
+    );
+    const calculatedY = easeOutQuint(
+      linearProgress,
+      props.translateY[0],
+      props.translateY[1] - props.translateY[0],
+      1
+    );
+    const circle = new Path2D();
+
+    circle.arc(
+      calculatedX * SCALE,
+      calculatedY * SCALE,
+      1 * SCALE,
+      0,
+      Math.PI * 2
+    );
+    ctx.fill(circle);
+  });
 }
 
 export function init(data) {
