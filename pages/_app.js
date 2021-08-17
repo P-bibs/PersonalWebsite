3357d758fb8a7339730c271da0a6996088c9cf5f
Paul Biberstein <biberstein.paul@gmail.com>
2021-08-16 22:53:50 -0400

    Add syntax highlighting to blog code

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import "../styles/tailwind.css";
+import 'highlight.js/styles/lioshi.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
