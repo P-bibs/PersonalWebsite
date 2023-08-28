14b0c0ab8c00cf40eb47defed7ae3831925fd522
Paul Biberstein <biberstein.paul@gmail.com>
2023-08-27 22:45:37 -0400

    Overhaul UI with "window"-based design

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -1,19 +1,25 @@
 module.exports = {
   content: [
-      '_site/**/*.html',
-      '_site/**/*.njk',
-      '_includes/**/*.njk',
-      '_includes/**/*.njk',
+    '_site/**/*.html',
+    '_site/**/*.njk',
+    '_includes/**/*.njk',
+    '_includes/**/*.njk',
   ],
   safelist: [],
   theme: {
     extend: {
+      boxShadow: {
+        'hard': '4px 4px 0 0 var(--tw-shadow-color)'
+      },
+      screens: {
+        '-md': { max: '767px' },
+      },
       colors: {
         change: 'transparent',
       },
     },
   },
   plugins: [
-      require('@tailwindcss/typography'),
+    require('@tailwindcss/typography'),
   ],
 }
